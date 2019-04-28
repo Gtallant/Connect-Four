48722a0eda1c422d8ea083389f2e7bcd80be77d5
Griffin Tallant <griffin.tallant@gmail.com>
2019-04-28 08:52:34 -0600

    Added start screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './App.scss';
 import Board from '../Board';
+import StartScreen from '../StartScreen';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Connect Four</h1>
-      </header>
-      <Board />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.startGame = this.startGame.bind(this);
+    this.state = {
+      screen: "START", // START | GAME | END
+      playerOneColor: null,
+    }
+  }
+
+  startGame(playerOneColor) {
+    this.setState({
+      screen: "GAME",
+      playerOneColor: playerOneColor,
+    });
+  }
+
+  render() {
+    const { screen } = this.state;
+    return (
+      <div className="App">
+        {screen === "START" ? <StartScreen start={this.startGame} /> : ''}
+        {screen === "GAME" ? <Board /> : ''}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/StartScreen/StartScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './StartScreen.scss';
+import Space from '../Space';
+
+class Board extends React.Component {
+  render() {
+    const { start } = this.props;
+  return (
+    <div className="start">
+      <h1 className="start__title"><img className="start__title__image" src="/assets/logo.png" alt="Connect 4" /></h1>
+      <p className="start__directions">Player 1 <br />pick a color to begin</p>
+      <div className="start__pick-color">
+        <button onClick={() => start('red')} className="start__pick-color__btn red">Red</button>
+        <button onClick={() => start('black')} className="start__pick-color__btn black">Black</button>
+      </div>
+    </div>
+  );
+  }
+}
+
+export default Board;
